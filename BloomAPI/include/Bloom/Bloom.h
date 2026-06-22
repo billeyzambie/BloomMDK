d56368ea1132c465f1e986b209bafb4de98e2e2e
Bill <52599981+billeyzambie@users.noreply.github.com>
2026-06-22 20:24:24 +0300

    Removed the bloom api from git tracking

--- BloomAPI/include/Bloom/Bloom.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#ifdef BLOOM_EXPORT
-#define BLOOM_API __declspec(dllexport)
-#define BLOOM_GLOBAL BLOOM_API inline
-#else
-#define BLOOM_API __declspec(dllimport)
-#define BLOOM_GLOBAL BLOOM_API extern
-#endif
