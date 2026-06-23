0aac961cf73ca3b3ac25b1df99fafadb293f8249
Bill <52599981+billeyzambie@users.noreply.github.com>
2026-06-23 13:48:16 +0300

    renamed the examplemod folder to src

--- src/ExampleMod.cpp
+++ src/ExampleMod.cpp
@@ -4,10 +4,17 @@
 #include "ExampleMod.h"
 
 #include <Plant.h>
+#include <Events.h>
+#include <EventContexts.h>
 
 extern "C" __declspec(dllexport) void ModInit(const std::string* theDllName)
 {
 	std::cout << "Hello from mod " << *theDllName << "! MSVC version : " << _MSC_VER << std::endl;
 
-	std::cout << Plant::GetNameString(SEED_SNOWPEA)	<< " eaten!" << std::endl;
+	std::cout << Plant::GetNameString(SEED_SNOWPEA)	<< " says hi!" << std::endl;
+
+	Events::Subscribe(+[](PlantEatenContext& theContext)
+		{
+			std::cout << Plant::GetNameString(theContext.mEatenPlant->mSeedType) << " eaten!" << std::endl;
+		});
 }
