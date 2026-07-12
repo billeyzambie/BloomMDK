e8ff14857c6eb3e2bd45f4ca9e12c946e6a87369
Bill <52599981+billeyzambie@users.noreply.github.com>
2026-07-13 00:17:26 +0300

    Added a custom store item

--- src/ExampleMod.cpp
+++ src/ExampleMod.cpp
@@ -1,24 +1,43 @@
-﻿// ExampleMod.cpp : Defines the entry point for the application.
-//
+﻿#include <iostream>
 
-#include "ExampleMod.h"
+#include <StoreItem/StoreItemTypes.h>
+#include <Registries.h>
 
-#include <Plant.h>
-#include <Events.h>
-
+#include <TodFoley.h>
 #include <LawnApp.h>
 #include <Board.h>
-#include <TodFoley.h>
+#include <PlantEatenContext.h>
+#include <Events.h>
+
+const RegistryTypeHolder<StoreItemType>& BRAIN = Registries::STORE_ITEMS.Register([]() {
+	StoreItemAttributes anAttributes;
+	anAttributes.mCost = 200;
+	anAttributes.mDrawOffsetX = 9;
+	anAttributes.mDrawOffsetY = 37;
+	anAttributes.mIcon = Sexy::IMAGE_BRAIN_ID;
+	anAttributes.mBuyCount = 2;
+	anAttributes.mMaxBuyCount = 10;
+	StoreItemType* aStoreItemType = new StoreItemType("examplemod", "brain", anAttributes);
+	return aStoreItemType;
+});
 
 extern "C" __declspec(dllexport) void ModInit(const std::string* theDllName)
 {
 	std::cout << "Hello from mod " << *theDllName << "! MSVC version : " << _MSC_VER << std::endl;
-
-	std::cout << Plant::GetNameString(SEED_SNOWPEA)	<< " says hi" << std::endl;
+	ReplaceStoreItemSpot(3, 6, BRAIN);
 
 	Events::Subscribe(+[](PlantEatenContext& theContext)
 		{
 			auto* aPlant = theContext.mEatenPlant;
+
+			PlayerInfo* aPlayerInfo = aPlant->mApp->mPlayerInfo;
+
+			StoreItem& aBrainStoreItemData = aPlayerInfo->GetStoreItemData(BRAIN);
+
+			if (aBrainStoreItemData.mPurchases <= 0)
+				return;
+
+			aBrainStoreItemData.mPurchases--;
 
 			int aPosX = aPlant->mX + aPlant->mWidth / 2;
 			int aPosY = aPlant->mY + aPlant->mHeight / 2;
